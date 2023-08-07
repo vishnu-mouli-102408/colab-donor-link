1e2ba32e797062e6d1389d4d867f363771e77d0f
Ganivada Mouli <vishnumouli0@gmail.com>
2023-08-07 22:31:35 +0530

    updated mongodb url and connected to database

--- donor-link-server/index.js
+++ donor-link-server/index.js
@@ -1,33 +1,38 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import helmet from 'helmet';
-import morgan from 'morgan';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import { dataUser } from "./data.index.js";
+import User from "./models/User.js";
 
 //Server Configuration
-dotenv.config()
+dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-
 //Routes
-app.get('/', (req, res) => {
-    res.send('Im getting there!')
-})
+app.get("/", (req, res) => {
+  res.send("Im getting there!");
+});
 
-//Mongoose Setup 
+//Mongoose Setup
 const PORT = process.env.PORT || 9000;
-mongoose.connect(process.env.MONGO_URL, {
+mongoose
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(() => {
-    app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
-}).catch((error) => console.log(`${error} did not connect`))
+  })
+  .then(() => {
+    User.insertMany(dataUser);
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
